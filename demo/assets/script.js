96252550e6982735e943d74a4626ceeee9a74d14
Julien Monty <julien.monty@gmail.com>
2014-03-02 22:16:09 +0100

    Add some controls to demo

--- demo/assets/script.js
+++ demo/assets/script.js
@@ -1,8 +1,23 @@
-var phonecatApp = angular.module('devicon', []);
+var devicon = angular.module('devicon', []);
  
-phonecatApp.controller('IconListCtrl', function ($scope, $http) {
+devicon.controller('IconListCtrl', function ($scope, $http) {
+
   $http.get('assets/devicon.json').success(function(data) {
     $scope.icons = data[0];
   });
- 
+
+});
+
+devicon.filter('toArray', function () {
+    'use strict';
+
+    return function (obj) {
+        if (!(obj instanceof Object)) {
+            return obj;
+        }
+
+        return Object.keys(obj).map(function (key) {
+            return Object.defineProperty(obj[key], '$key', {__proto__: null, value: key});
+        });
+    }
 });
