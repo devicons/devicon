6d65ca9b5d9500ccad2ee33aa1df1b174789493a
Jeremy Zahner <zahner@joshmartin.ch>
2015-09-10 13:12:06 +0200

    Adaption to automated bower workflow.

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,15 @@ var concatCss = require('gulp-concat-css');
 var plumber = require('gulp-plumber');
 
 gulp.task('concat-css', function () {
-  gulp.src(['./devicon.css', './devicon-colors.css'])
+  gulp.src(['./css/devicon.css', './css/devicon-colors.css'])
   .pipe(plumber())
-  .pipe(concatCss('./devicon.min.css'))
+  .pipe(concatCss('./css/devicon.min.css'))
   .pipe(gulp.dest('./'));
 });
 
 gulp.task('minify-css', function() {
-  gulp.src('./devicon.min.css')
+  gulp.src('./css/devicon.min.css')
   .pipe(plumber())
   .pipe(minifyCSS())
-  .pipe(gulp.dest('./'))
+  .pipe(gulp.dest('./css'))
 });
