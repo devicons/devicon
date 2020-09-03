43b5e98091cdb61c2b1922a35f8162a6af3feac2
Thomas Bui <thomasbui198@gmail.com>
2020-09-02 21:26:00 -0700

    Fix a bug with the workflow

--- .github/scripts/build_assets/PathResolverAction.py
+++ .github/scripts/build_assets/PathResolverAction.py
@@ -5,12 +5,15 @@ from pathlib import Path
 class PathResolverAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         path = Path(values).resolve()
-        if not self.dest == "download_path":
-            if not path.exists():
-                raise ValueError(f"{path} doesn't exist.")
+        if not path.exists():
+            raise ValueError(f"{path} doesn't exist.")
 
         if self.dest == "icons_folder_path":
             if not path.is_dir():
                 raise ValueError("icons_folder_path must be a directory")
+            
+        elif self.dest == "download_path":
+            if not path.is_dir():
+                raise ValueError("download_path must be a directory")
 
         setattr(namespace, self.dest, str(path))
