16f397eef5a359d25f16fc449dc61a8f982e3335
Thomas Bui <thomasbui198@gmail.com>
2020-09-04 09:50:31 -0700

    Revert "Change workflow file to upload geckodriver.log"
    This reverts commit dc226bad00bc4d5963bff6eb5e7bbccff4fbb8d5.

--- .github/scripts/icomoon_upload.py
+++ .github/scripts/icomoon_upload.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 from argparse import ArgumentParser
-from selenium.common.exceptions import TimeoutException
 
 # pycharm complains that build_assets is an unresolved ref
 # don't worry about it, the script still runs
@@ -43,26 +42,20 @@ def main():
         print("No files need to be uploaded. Ending script...")
         return
 
-    try:
-        runner = SeleniumRunner(args.icomoon_json_path, args.download_path,
-                                args.geckodriver_path, args.headless)
-        runner.upload_icomoon()
-        svgs = filehandler.get_svgs_paths(new_icons, args.icons_folder_path)
-        runner.upload_svgs(svgs)
+    runner = SeleniumRunner(args.icomoon_json_path, args.download_path,
+                            args.geckodriver_path, args.headless)
+    runner.upload_icomoon()
+    svgs = filehandler.get_svgs_paths(new_icons, args.icons_folder_path)
+    runner.upload_svgs(svgs)
 
 
-        zip_name = "devicon-v1.0.zip"
-        zip_path = Path(args.download_path, zip_name)
-        runner.download_icomoon_fonts(zip_path)
-        filehandler.extract_files(str(zip_path), args.download_path)
-        filehandler.rename_extracted_files(args.download_path)
-        runner.close()
-        print("Task completed.")
-    except TimeoutException as e:
-        print(e)
-        print(e.stacktrace)
-        runner.close()
-        return
+    zip_name = "devicon-v1.0.zip"
+    zip_path = Path(args.download_path, zip_name)
+    runner.download_icomoon_fonts(zip_path)
+    filehandler.extract_files(str(zip_path), args.download_path)
+    filehandler.rename_extracted_files(args.download_path)
+    runner.close()
+    print("Task completed.")
 
 
 if __name__ == "__main__":
