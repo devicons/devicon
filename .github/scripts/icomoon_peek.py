1e6ff7cd96d67f9818c669e36ddf9f4a1f5dbbff
Thomas Bui <thomasbui198@gmail.com>
2020-12-29 10:16:55 -0800

    Add fallback functionality if no icons found matching title

--- .github/scripts/icomoon_peek.py
+++ .github/scripts/icomoon_peek.py
@@ -12,9 +12,6 @@ from build_assets import filehandler, util
 def main():
     args = util.get_commandline_args(True)
     new_icons = filehandler.find_new_icons(args.devicon_json_path, args.icomoon_json_path)
-    if len(new_icons) == 0:
-        print("No files need to be uploaded. Ending script...")
-        return
 
     # get only the icon object that has the name matching the pr title
     filtered_icons = find_object_added_in_this_pr(new_icons, args.pr_title)
@@ -23,11 +20,19 @@ def main():
     print("List of new icons:", *new_icons, sep = "\n")
     print("Icons being uploaded:", *filtered_icons, sep = "\n")
 
+    if len(new_icons) == 0:
+        print("No files need to be uploaded. Ending script...")
+        return
+
+    screenshot_folder = filehandler.create_screenshot_folder("./") 
+    if len(filtered_icons) == 0:
+        print("No icons found matching the icon name in the PR's title. Fallback to uploading all new icons found.")
+        screenshot_folder = ""
+
     runner = None
     try:
         runner = SeleniumRunner(args.download_path, args.geckodriver_path, args.headless)
         svgs = filehandler.get_svgs_paths(new_icons, args.icons_folder_path)
-        screenshot_folder = filehandler.create_screenshot_folder("./") 
         runner.upload_svgs(svgs, screenshot_folder)
         print("Task completed.")
     except TimeoutException as e:
