a9eb51aca51884c3b2640c520e7c2070b3ced6d3
Thomas Bui <thomasbui198@gmail.com>
2020-12-29 21:49:39 -0800

    Various bug fixes and added ability to comment multiple files

--- .github/scripts/generate_screenshot_markdown.py
+++ .github/scripts/generate_screenshot_markdown.py
@@ -1,19 +1,10 @@
-from typing import List
-
-from build_assets import arg_getters
-
-
-def generate_screenshot_markdown(img_urls: List[str]):
-    """
-    Generate the markdown for the screenshots using the
-    img_urls then print it to the console.
-    :param img_urls are valid image links.
-    """
-    template = "![Detailed Screenshot]({})"
-    return [template.format(img_url) for img_url in img_urls]
+import json
+import os
 
 
 if __name__ == "__main__":
-    args = arg_getters.get_generate_markdown_args()
-    markdown = generate_screenshot_markdown(args.img_urls)
-    print("\n\n".join(markdown))  # format it before printing
+    img_urls_list = json.loads(os.environ["IMG_URLS"])
+    template = "![Detailed Screenshot]({})"
+    markdown = [template.format(img_url) for img_url in img_urls_list]
+    print("\n\n".join(markdown))
+
