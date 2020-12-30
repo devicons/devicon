82ffc13995317c1f4d2bc0b61c0ad8b42cb63104
Thomas Bui <thomasbui198@gmail.com>
2020-12-29 17:49:23 -0800

    Added logging

--- .github/scripts/generate_screenshot_markdown.py
+++ .github/scripts/generate_screenshot_markdown.py
@@ -10,13 +10,14 @@ def generate_screenshot_markdown(img_urls: List[str]):
     img_urls then print it to the console.
     :param img_urls are valid image links.
     """
+    print("Generating markdowns...")
     template = "![Detailed Screenshot]({})"
     return [template.format(img_url) for img_url in img_urls]
 
 
 if __name__ == "__main__":
     args = arg_getters.get_generate_markdown_args()
-    print(args.img_urls)
+    print("Img urls: ", args.img_urls)
     img_urls_list = json.loads(args.img_urls)
     markdown = generate_screenshot_markdown(img_urls_list)
     print("\n\n".join(markdown))  # format it before printing
