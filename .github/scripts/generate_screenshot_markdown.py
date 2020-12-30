c91c249c19323a23e643cc9107dd1df0b6ca34f6
Thomas Bui <thomasbui198@gmail.com>
2020-12-29 19:57:51 -0800

    Now write env var into GITHUB_ENV

--- .github/scripts/generate_screenshot_markdown.py
+++ .github/scripts/generate_screenshot_markdown.py
@@ -1,23 +1,10 @@
-from typing import List
 import json
 import os
-
-from build_assets import arg_getters
-
-
-def generate_screenshot_markdown(img_urls: List[str]):
-    """
-    Generate the markdown for the screenshots using the
-    img_urls then print it to the console.
-    :param img_urls are valid image links.
-    """
-    template = "![Detailed Screenshot]({})"
-    return [template.format(img_url) for img_url in img_urls]
 
 
 if __name__ == "__main__":
     img_urls_list = json.loads(os.environ["IMG_URLS"])
-    markdown = generate_screenshot_markdown(img_urls_list)
-    os.environ["DETAILED_IMGS_MARKDOWN"] = "\n\n".join(markdown)
-    print(os.environ["DETAILED_IMGS_MARKDOWN"])
+    template = "![Detailed Screenshot]({})"
+    markdown = [template.format(img_url) for img_url in img_urls_list]
+    print("\n\n".join(markdown))
 
