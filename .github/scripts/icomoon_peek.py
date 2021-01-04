1a0ec51ba7e3839843c00a8e406448fff38f036d
Thomas Bui <thomasbui198@gmail.com>
2021-01-04 15:13:28 -0800

    Added check for height and width attr

--- .github/scripts/icomoon_peek.py
+++ .github/scripts/icomoon_peek.py
@@ -70,8 +70,9 @@ def find_object_added_in_this_pr(icons: List[dict], pr_title: str):
 
 def check_svgs(svg_file_paths: List[str]):
     """
-    Check the viewBox and style of each svgs passed in.
+    Check the width, height, viewBox and style of each svgs passed in.
     The viewBox must be '0 0 128 128'.
+    If the svg has a width and height attr, ensure it's '128px'.
     The style must not contain any 'fill' declarations.
     If any error is found, they will be thrown.
     :param: svg_file_paths, the file paths to the svg to check for.
@@ -86,8 +87,15 @@ def check_svgs(svg_file_paths: List[str]):
         if root.tag != f"{namespace}svg":
             err_msgs.append(f"Root is '{root.tag}'. Root must be an 'svg' element: {svg_path}")
 
-        if root.attrib["viewBox"] != "0 0 128 128":
+        if root.get("viewBox") != "0 0 128 128":
             err_msgs.append("SVG 'viewBox' is not '0 0 128 128': " + svg_path)
+
+        acceptable_size = [None, "128px", "128"]
+        if root.get("height") not in acceptable_size:
+            err_msgs.append("SVG 'height' is present but is not '128' or '128px': " + svg_path)
+
+        if root.get("width") not in acceptable_size:
+            err_msgs.append("SVG 'width' is present but is not '128' or '128px': " + svg_path)
 
         style = root.findtext(f".//{namespace}style")
         if style != None and "fill" in style:
