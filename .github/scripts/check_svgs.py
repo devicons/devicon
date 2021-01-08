e339961cf8e706a0b6ef3721f1a0f3abfa2a0ffe
Thomas Bui <thomasbui198@gmail.com>
2021-01-07 16:56:27 -0800

    Updated logging messages

--- .github/scripts/check_svgs.py
+++ .github/scripts/check_svgs.py
@@ -84,7 +84,7 @@ def check_svgs(svg_file_paths: List[Path]):
             err_msgs.append("\n".join(err_msg))
 
     if len(err_msgs) > 0:
-        raise Exception("\n" + "\n\n".join(err_msgs))
+        raise Exception("\n\n".join(err_msgs))
 
 
 if __name__ == "__main__":
