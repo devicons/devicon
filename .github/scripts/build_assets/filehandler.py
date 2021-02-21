8a83c8f58cce4ca759f2589062f37ab3db6061fa
amacado <clemens.bastian@gmail.com>
2021-02-22 00:16:26 +0100

    try path.root to check directory

--- .github/scripts/build_assets/filehandler.py
+++ .github/scripts/build_assets/filehandler.py
@@ -221,12 +221,12 @@ def get_added_modified_svgs(files_added_json_path: str,
     svgs = []
     for file in files_added:
         path = Path(file)
-        if path.suffix.lower() == ".svg" and path.is_relative_to('icons'):
+        if path.suffix.lower() == ".svg" and path.root.lower() == 'icons':
             svgs.append(path)
 
     for file in files_modified:
         path = Path(file)
-        if path.suffix.lower() == ".svg" and path.is_relative_to('icons'):
+        if path.suffix.lower() == ".svg" and path.root.lower() == 'icons':
             svgs.append(path)
     
     return svgs
