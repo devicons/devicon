7499f08e23104fd3bc3a888ffd49bc36a96f1ce2
amacado <clemens.bastian@gmail.com>
2021-02-22 00:39:00 +0100

    add check for directory /icons

--- .github/scripts/build_assets/filehandler.py
+++ .github/scripts/build_assets/filehandler.py
@@ -221,14 +221,12 @@ def get_added_modified_svgs(files_added_json_path: str,
     svgs = []
     for file in files_added:
         path = Path(file)
-        print(path.as_posix())
-        if path.suffix.lower() == ".svg":
+        if path.suffix.lower() == ".svg" and path.as_posix().lower().startswith('icons/'):
             svgs.append(path)
 
     for file in files_modified:
         path = Path(file)
-        print(path.as_posix())
-        if path.suffix.lower() == ".svg":
+        if path.suffix.lower() == ".svg" and path.as_posix().lower().startswith('icons/'):
             svgs.append(path)
     
     return svgs
