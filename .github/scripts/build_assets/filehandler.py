0e8faecfc0668104e130bdbe25c106aae4694a87
amacado <clemens.bastian@gmail.com>
2021-02-22 00:32:19 +0100

    add debug vars

--- .github/scripts/build_assets/filehandler.py
+++ .github/scripts/build_assets/filehandler.py
@@ -221,11 +221,13 @@ def get_added_modified_svgs(files_added_json_path: str,
     svgs = []
     for file in files_added:
         path = Path(file)
+        print("SVGs to check: ", path.parents, sep='\n')
         if path.suffix.lower() == ".svg" and "icons/" in path.parents:
             svgs.append(path)
 
     for file in files_modified:
-        path = Path(file)
+        print("SVGs to check: ", path.parents, sep='\n')
+        print(path.parents)
         if path.suffix.lower() == ".svg" and "icons/" in path.parents:
             svgs.append(path)
     
