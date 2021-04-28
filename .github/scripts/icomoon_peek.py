001b136bf5e4bd3f6d81f129ba156066e1e442fe
Thomas Bui <thomasbui198@gmail.com>
2021-04-27 19:23:41 -0700

    Reformat devicon object error messages

--- .github/scripts/icomoon_peek.py
+++ .github/scripts/icomoon_peek.py
@@ -66,52 +66,52 @@ def check_devicon_object(icon: dict, icon_name: str):
     """
     err_msgs = []
     try:
-        if type(icon["name"]) != icon_name:
-            err_msgs.append("'name' value is not: " + icon_name)
+        if icon["name"] != icon_name:
+            err_msgs.append("- 'name' value is not: " + icon_name)
     except KeyError:
-        err_msgs.append("Missing key: 'name'.")
+        err_msgs.append("- missing key: 'name'.")
 
     try:
         for tag in icon["tags"]:
             if type(tag) != str:
                 raise TypeError()
     except TypeError:
-        err_msgs.append("'tags' must be an array of strings, not: " + str(icon["tags"]))
+        err_msgs.append("- 'tags' must be an array of strings, not: " + str(icon["tags"]))
     except KeyError:
-        err_msgs.append("Missing key: 'tags'.")
+        err_msgs.append("- missing key: 'tags'.")
 
     try:
         if type(icon["versions"]) != dict:
-            err_msgs.append("'versions' must be an object.")
+            err_msgs.append("- 'versions' must be an object.")
     except KeyError:
-        err_msgs.append("Missing key: 'versions'.")
+        err_msgs.append("- missing key: 'versions'.")
 
     try:
         if type(icon["versions"]["svg"]) != list or len(icon["versions"]["svg"]) == 0:
-            err_msgs.append("Must contain at least 1 svg version in a list.")
+            err_msgs.append("- must contain at least 1 svg version in a list.")
     except KeyError:
-        err_msgs.append("Missing key: 'svg' in 'versions'.")
+        err_msgs.append("- missing key: 'svg' in 'versions'.")
     
     try:
         if type(icon["versions"]["font"]) != list or len(icon["versions"]["svg"]) == 0:
-            err_msgs.append("Must contain at least 1 font version in a list.")
+            err_msgs.append("- must contain at least 1 font version in a list.")
     except KeyError:
-        err_msgs.append("Missing key: 'font' in 'versions'.")
+        err_msgs.append("- missing key: 'font' in 'versions'.")
 
     try:
         if type(icon["color"]) != str or "#" not in icon["color"]:
-            err_msgs.append("'color' must be a string in the format '#abcdef'")
+            err_msgs.append("- 'color' must be a string in the format '#abcdef'")
     except KeyError:
-        err_msgs.append("Missing key: 'color'.")
+        err_msgs.append("- missing key: 'color'.")
 
     try:
         if type(icon["aliases"]) != list:
-            err_msgs.append("'aliases' must be an array.")
+            err_msgs.append("- 'aliases' must be an array.")
     except KeyError:
-        err_msgs.append("Missing key: 'aliases'.")
+        err_msgs.append("- missing key: 'aliases'.")
     
     if len(err_msgs) > 0:
-        message = "Error found in 'devicon.json' for '{}' entry: \n{}\n".format(icon_name, "\n".join(err_msgs))
+        message = "Error found in 'devicon.json' for '{}' entry: \n{}".format(icon_name, "\n".join(err_msgs))
         raise ValueError(message)
     return ""
 
