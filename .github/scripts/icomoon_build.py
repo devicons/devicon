39070daf2320c3f74d2422a1c56e2d1844620fd0
Thomas Bui <43018778+Thomas-Boi@users.noreply.github.com>
2021-12-26 20:25:49 -0800

    Update .github/scripts/icomoon_build.py
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- .github/scripts/icomoon_build.py
+++ .github/scripts/icomoon_build.py
@@ -49,7 +49,7 @@ def main():
         print("Creating the release message by querying the GitHub API...")
         get_release_message(args.token)
 
-        print("Closing the issues with the label of `in-develop`.")
+        print("Closing issues with the `in-develop` label.")
         issues = api_handler.get_issues_by_labels(args.token, ["in-develop"])
         issue_nums = [issue_num["number"] for issue_num in issues]
         api_handler.close_issues(args.token, issue_nums)
